85179b4baa7fe419bfde85d3c0becaf19db58609
kbcruz6 <agustintcruz@gmail.com>
2022-10-06 11:27:13 -0300

    asd

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require ("path");
 
 app.use(express.urlencoded({extended:false}));
+
 app.use(express.json());
 
 app.use(require("./src/routes/mails.js"));
@@ -12,5 +13,5 @@ app.use(express.static(path.join(__dirname, "/")));
 const PUERTO = process.env.PORT || 3000;
 
 app.listen(PUERTO, () =>{
-    console.log("Server en puerto 3000 king")
+    console.log(`Server en puerto ${PUERTO} king`)
 });
