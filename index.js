123f57bb812de0a2a28d19f075168bce700b4c7c
kbcruz6 <agustintcruz@gmail.com>
2022-10-06 11:05:19 -0300

    Funcionara?

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require ("path");
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.use(require("./src/routes/mails.js"));
+
+app.use(express.static(path.join(__dirname, "/")));
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, () =>{
+    console.log("Server en puerto 3000 king")
+});
