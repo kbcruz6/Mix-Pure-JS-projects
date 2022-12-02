a9f3552a35a9e5fd8aee68fcf0e2c2c2fe6667ae
kbcruz6 <agustintcruz@gmail.com>
2022-12-02 16:04:06 -0300

    2652

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 //! Routing
-app.use(express.static("/"));
+app.use(express.static(path.join(__dirname, "/")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //! Create GET request
-// app.get("/", (req, res) => {
-//   res.send("Express on Vercel");
-// });
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
 //! Routers
 const routerMails = require("./src/routes/mails.js");
