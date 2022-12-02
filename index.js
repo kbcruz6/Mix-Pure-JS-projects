359c78f9c11734974c834c10dbeaf946209ed928
kbcruz6 <agustintcruz@gmail.com>
2022-12-02 14:30:37 -0300

    321

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
 // Routers
 const routerMails = require("./src/routes/mails.js");
 app.use(routerMails);
 
-const PUERTO = process.env.PORT || 3000;
+const PUERTO = process.env.PORT || 5000;
 
 app.listen(PUERTO, () => {
   console.log(`Server en puerto ${PUERTO} king`);
 });
+
+// Export the Express API
+module.exports = app;
