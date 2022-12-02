471199578a8093d78c11302d1c6f45d8a47d1744
kbcruz6 <agustintcruz@gmail.com>
2022-12-02 13:01:07 -0300

    23

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require ('path');
-const bodyParser = require('body-parser');
-
+const path = require("path");
+const bodyParser = require("body-parser");
 
 // Routing
 app.use(express.static(path.join(__dirname + "/")));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
 
 // Routers
-const routerMails = require ('./src/routes/mails.js')
+const routerMails = require("./src/routes/mails.js");
 app.use(routerMails);
-
 
 const PUERTO = process.env.PORT || 3000;
 
-app.listen(PUERTO, () =>{
-    console.log(`Server en puerto ${PUERTO} king`)
+app.listen(PUERTO, () => {
+  console.log(`Server en puerto ${PUERTO} king`);
 });
